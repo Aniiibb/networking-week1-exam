cd9ef0f37130db488ed50f067cf46735e224c0ba
Anib <trainee.anib.mhamud@gmail.com>
2026-03-13 15:42:11 +0000

    task 2 with some errors

--- postcode_cli.py
+++ postcode_cli.py
@@ -3,4 +3,26 @@
 from argparse import ArgumentParser
 from postcode_functions import validate_postcode, get_postcode_completions
 if __name__ == "__main__":
-    parser = Argument
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--mode", "m",
+        required=True,
+        choices=["validate", "complete"]
+    )
+    parser.add_argument("postcode")
+    args = parser.parse_args()
+
+    postcode = args.postcode.upper().strip()
+    if args.mode == "validate":
+        if validate_postcode(postcode):
+            print(f"{postcode} is a valid postcode")
+        else:
+            print(f"{postcode} is not a valid postcode")
+
+    elif args.mode == "complete":
+        completions = get_postcode_completions(postcode)
+        if len(completions) == 0:
+            print(f"No matches for {postcode}.")
+        else:
+            for completion in completions[:5]:
+                print(completion)
